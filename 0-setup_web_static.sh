aa501b156f4ac37a5bdaf11067fb741968bafbe8
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-10-05 23:45:17 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,39 @@
 #!/usr/bin/env bash
-# Setting up a web servers for the deployment of web_static.
+# Seting up a web server for the deployment of web_static.
 
-apt update -y
-apt install -y nginx
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<!DOCTYPE html>
-<html>
+echo "<html>
   <head>
   </head>
   <body>
-    <p>Nginx server test</p>
+    Holberton School
   </body>
-</html>" | tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-sudo service nginx restart
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
