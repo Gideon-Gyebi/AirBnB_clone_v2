f60abf59c434e1e64c9e21f8a0ff4819d94b3eb4
Gideon Gyebi <gyebigideon000@gmail.com>
2023-10-05 17:09:43 -0700

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static folder."""
-from fabric.api import local
-import time
+"""Fabric script generating a .tgz archive from contents of the web_static"""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
-    """Generate a .tgz archive from web_static folder"""
+    """Archiving the web static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        a_p = "versions/web_static_{}.tgz".format(time.
-                                                  strftime("%Y%m%d%H%M%S"))
-        return a_p
-    except Exception as e:
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
